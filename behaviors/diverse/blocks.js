b71064b4c8d2d6ad6edbbdf25176a95eb3276c9b
wwj718 <wuwenjie718@gmail.com>
2022-12-21 21:42:28 +0800

    use BlocksEditorActor tick

--- behaviors/diverse/blocks.js
+++ behaviors/diverse/blocks.js
@@ -88,6 +88,23 @@ class SpriteManagerPawn {
     }
 }
 
+class BlocksEditorActor {
+
+    tick(){
+        let spriteName = `${this.name}-${this.id}`;
+        // Make sure world already exists
+        if (window.world){
+            let ide = window.world.children[0];
+            // List from Snap! list.js
+            let payload = new List([spriteName, new List(this.translation), new List(this.rotation), new List(this.scale)]);
+            ide.broadcast("updateCardData", null, payload);
+        }
+        // sent together will cause overwriting. This is a temporary solution
+        this.future(20 + 20 * this.random()).tick();
+    }
+    
+}
+
 class BlocksEditorPawn {
     setEditor() {
         this.subscribe("blocks", "setPropertyTo", this.setPropertyTo);
@@ -98,21 +115,6 @@ class BlocksEditorPawn {
         const sprite = ide.sprites.asArray().filter((morph) => morph.name === spriteName)[0];
         ide.selectSprite(sprite);
         editor.style.display = "";
-
-        this.tick();
-    }
-
-    tick(){
-        let spriteName = `${this.actor.name}-${this.actor.id}`;
-        // Make sure world already exists
-        if (window.world){
-            let ide = window.world.children[0];
-            // List from Snap! list.js
-            let payload = new List([spriteName, new List(this.actor.translation), new List(this.actor.rotation), new List(this.actor.scale)]);
-            ide.broadcast("updateCardData", null, payload);
-        }
-        // sent together will cause overwriting. This is a temporary solution
-        this.future(20 + 20 * this.random()).tick();
     }
 
     setPropertyTo(data) {
@@ -140,6 +142,7 @@ export default {
         },
         {
             name: "BlocksEditor",
+            actorBehaviors: [BlocksEditorActor],
             pawnBehaviors: [BlocksEditorPawn]
         }
     ]
